9825d06728bad4edfc8c02fa1b3980c0dda38851
Gabrielle Crevecoeur <gcrev93@gmail.com>
2020-11-15 19:46:45 -0500

    uploading container.js solution

--- src/components/Container.js
+++ src/components/Container.js
@@ -21,40 +21,83 @@ export default function Container() {
   ////////////// NETWORK HELPERS //////////////
   ////////////// NETWORK HELPERS //////////////
   const getQuotes = () => {
-    console.log(`TASK 1- Use 'axios' or 'fetch' to [GET] a list of quotes
-      from 'http://localhost:3333/api/quotes'. On success, the quotes
-      in the response body should be set as the 'quotes' slice of state.
-      On error, 'handleError' should be called.`)
+
+    axios.get(quotesURL)
+      .then(res => {
+        setQuotes(quotes => quotes.concat(res.data))
+      })
+      .catch(err => {
+        handleError(err)
+      })
+
+
   }
 
   const postQuote = ({ text, author }) => {
-    console.log(`TASK 2- Use 'axios' or 'fetch' to [POST] a new quote
-      in 'http://localhost:3333/api/quotes'. On success, the new quote
-      in the response body should be added to the 'quotes' slice of state.
-      On error, 'handleError' should be called. Finally, the form should be reset.`)
+
+    axios.post(quotesURL, {
+      text: text, author: author
+    })
+      .then(res => {
+        setQuotes(quotes => quotes.concat(res.data))
+      })
+      .catch(err => {
+        handleError(err)
+      })
+
+    resetForm()
   }
 
   const putQuote = ({ id, text, author }) => {
-    console.log(`TASK 3- Use 'axios' or 'fetch' to [PUT] an existing quote
-      in 'http://localhost:3333/api/quotes/:id'. On success, the updated quote
-      in the response body should be used to replace the old version of the quote in 'quotes'.
-      On error, 'handleError' should be called. Finally, the form should be reset.`)
+
+
+    axios.put(quotesURL + '/' + id, {
+      text: text, author: author
+    })
+      .then(res => {
+        let updatedQuotes = quotes.map(q => {
+          if (q.id === id) {
+            q.text = res.data.text
+            q.author = res.data.author
+          }
+
+          return q
+        })
+
+        setQuotes(updatedQuotes)
+      })
+      .catch(err => {
+        handleError(err)
+      })
+
+    resetForm()
   }
 
   const deleteQuote = (id) => {
-    console.log(`TASK 4- Use 'axios' or 'fetch' to [DELETE] an existing quote
-      in 'http://localhost:3333/api/quotes/:id'. On success, the deleted quote
-      should be removed from the 'quotes' slice of state'.
-      On error, 'handleError' should be called. Finally, the form should be reset.`)
+
+    axios.delete(quotesURL + '/' + id)
+      .then(res => {
+        setQuotes(quotes => quotes.filter(q => q.id !== res.data))
+      })
+      .catch(err => {
+        handleError(err)
+      })
+
+    resetForm()
   }
 
   ////////////// OTHER HELPERS //////////////
   ////////////// OTHER HELPERS //////////////
   ////////////// OTHER HELPERS //////////////
   const editQuote = (id) => {
-    console.log(`TASK 5- This helper should find inside 'quotes' the quote with the given 'id'.
-      Use the 'id', 'text' and 'author' properties of this quote to populate the corresponding
-      fields of the 'formValues' slice of state.`)
+
+    const quote = quotes.find(q => q.id == id)
+
+    setFormValues({
+      id: quote.id,
+      text: quote.text,
+      author: quote.author
+    })
   }
 
   const handleError = err => { debugger } // eslint-disable-line
@@ -87,5 +130,143 @@ export default function Container() {
         reset={resetForm}
       />
     </div>
+
   )
+
+}
+import React, { useState, useEffect } from 'react'
+import Form from './Form'
+import axios from 'axios'
+
+const quotesURL = 'http://localhost:3333/api/quotes'
+
+const initialFormState = {
+  id: '',
+  text: '',
+  author: '',
+}
+
+export default function Container() {
+  ////////////// SLICES OF STATE //////////////
+  ////////////// SLICES OF STATE //////////////
+  ////////////// SLICES OF STATE //////////////
+  const [quotes, setQuotes] = useState([])
+  const [formValues, setFormValues] = useState(initialFormState)
+
+  ////////////// NETWORK HELPERS //////////////
+  ////////////// NETWORK HELPERS //////////////
+  ////////////// NETWORK HELPERS //////////////
+  const getQuotes = () => {
+
+    axios.get(quotesURL)
+      .then(res => {
+        setQuotes(quotes => quotes.concat(res.data))
+      })
+      .catch(err => {
+        handleError(err)
+      })
+
+
+  }
+
+  const postQuote = ({ text, author }) => {
+
+    axios.post(quotesURL, {
+      text: text, author: author
+    })
+      .then(res => {
+        setQuotes(quotes => quotes.concat(res.data))
+      })
+      .catch(err => {
+        handleError(err)
+      })
+
+    resetForm()
+  }
+
+  const putQuote = ({ id, text, author }) => {
+
+
+    axios.put(quotesURL + '/' + id, {
+      text: text, author: author
+    })
+      .then(res => {
+        let updatedQuotes = quotes.map(q => {
+          if (q.id === id) {
+            q.text = res.data.text
+            q.author = res.data.author
+          }
+
+          return q
+        })
+
+        setQuotes(updatedQuotes)
+      })
+      .catch(err => {
+        handleError(err)
+      })
+
+    resetForm()
+  }
+
+  const deleteQuote = (id) => {
+
+    axios.delete(quotesURL + '/' + id)
+      .then(res => {
+        setQuotes(quotes => quotes.filter(q => q.id !== res.data))
+      })
+      .catch(err => {
+        handleError(err)
+      })
+
+    resetForm()
+  }
+
+  ////////////// OTHER HELPERS //////////////
+  ////////////// OTHER HELPERS //////////////
+  ////////////// OTHER HELPERS //////////////
+  const editQuote = (id) => {
+
+    const quote = quotes.find(q => q.id == id)
+
+    setFormValues({
+      id: quote.id,
+      text: quote.text,
+      author: quote.author
+    })
+  }
+
+  const handleError = err => { debugger } // eslint-disable-line
+
+  const resetForm = () => setFormValues(initialFormState)
+
+  ////////////// SIDE EFFECTS //////////////
+  ////////////// SIDE EFFECTS //////////////
+  ////////////// SIDE EFFECTS //////////////
+  useEffect(() => getQuotes(), [])
+
+  return (
+    <div className='container'>
+      <h3>Quotes</h3>
+      <ul>
+        {
+          quotes.map((q, i) => (
+            <li key={q.id}>
+              <div>{q.text} ({q.author})</div>
+              <button onClick={() => editQuote(q.id)}>Edit</button>
+              <button onClick={() => deleteQuote(q.id)}>Delete</button>
+            </li>
+          ))
+        }
+      </ul>
+      <Form
+        values={formValues}
+        setValues={setFormValues}
+        submitHandlers={{ postQuote, putQuote }}
+        reset={resetForm}
+      />
+    </div>
+
+  )
+
 }
